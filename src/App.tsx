4b4e8162eaabdb7bfd62cc1319a9f73c3225ef7a
IndriesAndrei <indriesandrei@yahoo.co.uk>
2023-08-20 16:05:40 +0300

    Conditionally show the app UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css'
 
 const messages = [
@@ -7,25 +8,39 @@ const messages = [
 ];
 
 function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
-    alert('Test')
+    if(step > 1) {
+      setStep(step - 1);
+    }
+  }
+
+  function handleNext() {
+    if(step < 3) {
+      setStep(step + 1);
+    }
   }
 
   return (
-    <div className='steps'>
-      <h2>Steps App</h2>
-      <div className='numbers'>
-        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
-      </div>
-      <p className='message'>Step {step}: {messages[step - 1]}</p>
-      <div className='buttons'>
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious}>Previous</button>
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={() => alert('Next')}>Next</button>
-      </div>
+    <div>
+      <button className='close' onClick={() => setIsOpen(!isOpen)}>X</button>
+      { isOpen && (
+        <div className='steps'>
+          <h2>Steps App</h2>
+          <div className='numbers'>
+            <div className={step >= 1 ? 'active' : ''}>1</div>
+            <div className={step >= 2 ? 'active' : ''}>2</div>
+            <div className={step >= 3 ? 'active' : ''}>3</div>
+          </div>
+          <p className='message'>Step {step}: {messages[step - 1]}</p>
+          <div className='buttons'>
+            <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious}>Previous</button>
+            <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext}>Next</button>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
